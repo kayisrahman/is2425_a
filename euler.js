60161ad8d6d113053f97934e974141d61f19552e
kayisrahman <20035121@mydbs.ie>
2024-09-27 09:04:55 +0100

    added docal method

--- euler.js
+++ euler.js
@@ -1,12 +1,16 @@
 a = document.getElementById("userInputA").value;
 b = document.getElementById("userInputB").value;
 n = document.getElementById("userInputSum").value;
-sum = 0;
 
-for(let i = 0; i < n; i++){
-    if(i % a == 0 || i % b == 0){
-        sum+= i;
+
+function doCal() {
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (i % a == 0 || i % b == 0) {
+            sum += i;
+        }
     }
-}
 
-alert(sum);
+    alert(sum);
+}
