6f03e9729bbb55e513219ea98df3197ceacc4e91
kayisrahman <20035121@mydbs.ie>
2024-09-27 09:03:08 +0100

    euler ui version

--- euler.js
+++ euler.js
@@ -1,4 +1,7 @@
-a = 3, b = 5 , n = 1000, sum = 0;
+a = document.getElementById("userInputA").value;
+b = document.getElementById("userInputB").value;
+n = document.getElementById("userInputSum").value;
+sum = 0;
 
 for(let i = 0; i < n; i++){
     if(i % a == 0 || i % b == 0){
