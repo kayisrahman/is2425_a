ab9e12a7b2690fe32400c6f5c33fc70318391fc8
kayisrahman <20035121@mydbs.ie>
2024-09-27 08:47:46 +0100

    console logs

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-alert('Hello world');
-
-alert('The result of 1/3+1/3+3/4 is ' + (1/3+1/3+4/3));
-alert('The result of 4/3+1/3+1/3 is ' + (4/3+1/3+1/3));
+console.log('Hello world');
+console.log('The result of 1/3+1/3+3/4 is ' + (1/3+1/3+4/3));
+console.log('The result of 4/3+1/3+1/3 is ' + (4/3+1/3+1/3));
