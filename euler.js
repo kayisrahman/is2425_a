ce0c5f350b76377bdd363d0cf4b52b205667e31d
kayisrahman <20035121@mydbs.ie>
2024-10-07 10:31:11 +0100

    reveted the changes

--- euler.js
+++ euler.js
@@ -3,9 +3,9 @@ let doCalcForAss1 = (a, b, n) => {
     let sum = 0;
 
     for (let i = 0; i % a == 0 || i % b == 0; i++) {
-        // if (i % a == 0 || i % b == 0) {
+        if (i % a == 0 || i % b == 0) {
             sum += i;
-        // }
+        }
     }
 
     return sum;
