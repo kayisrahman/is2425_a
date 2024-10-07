f4c89a6dc6ee073865beedfc0c02b61d87e87c40
kayisrahman <20035121@mydbs.ie>
2024-10-07 10:29:20 +0100

    loop replaced with if

--- euler.js
+++ euler.js
@@ -2,10 +2,10 @@
 let doCalcForAss1 = (a, b, n) => {
     let sum = 0;
 
-    for (let i = 0; i < n; i++) {
-        if (i % a == 0 || i % b == 0) {
+    for (let i = 0; i % a == 0 || i % b == 0; i++) {
+        // if (i % a == 0 || i % b == 0) {
             sum += i;
-        }
+        // }
     }
 
     return sum;
