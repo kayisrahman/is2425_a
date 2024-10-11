318d6f6785d2997b10281064e8b28df8f6a71e55
kayisrahman <20035121@mydbs.ie>
2024-10-11 08:46:26 +0100

    undefined fix

--- basket.js
+++ basket.js
@@ -8,7 +8,7 @@ and prices is a mapping from products(String) to prices (number)
 and returns the total cost of the basket
 */
 let basket = {
-    "product": "Ball",
+    "product": "Eggs",
     "qty": 2
 }
 
@@ -16,11 +16,13 @@ let prices = {
     "priceBall": 4.99,
     "priceMilk": 1.05,
     "priceEggs": 3.50,
-    "priceEggs": 3.50,
 };
 let basketCost = (b, p) => {
     console.log(b, p);
-    return p["price" + b.product] * b.qty;
+    if (p["price" + b.product]) {
+        return p["price" + b.product] * b.qty;
+    }
+    return 0;
 }
 
 alert(basketCost(basket, prices));
