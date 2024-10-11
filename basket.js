2e94c1fc5e8dc533b90fceee5e6a1d15cd1c79c0
kayisrahman <20035121@mydbs.ie>
2024-10-11 09:12:14 +0100

    for each replced

--- basket.js
+++ basket.js
@@ -7,23 +7,25 @@ where basket is a mapping from products (string) to quantities (number)
 and prices is a mapping from products(String) to prices (number)
 and returns the total cost of the basket
 */
-let basket = {
-    "Eggs": 2,
+let basket = { // product to qty
+    "Eggs": 2, 
     "Milk": 4,
     "Ball": 1
 }
 
-let prices = {
+let prices = { // product to price
     "Ball": 4.99,
     "Milk": 1.05,
     "Eggs": 3.50,
 };
+
 let basketCost = (b, p) => {
     console.log(b, p);
     let sum = 0;
-    for (const [key, value] of Object.entries(b)) {
+   
+    for (let key in b) {
         if (p[key]) {
-            sum += value * p[key];
+            sum += b[key] * p[key];
         }
     }
     return sum;
